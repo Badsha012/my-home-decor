d6a3e2e6610f0b5b966d9a5e11364390161c0905
Badsha012 <badshasheikh361@gmail.com>
2025-10-06 21:26:28 +0600

    ten commit

---
+++ src/Layouts/Layouts.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import { Outlet } from 'react-router';
+
+const Layouts = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <h1>Content</h1>
+            <Footer></Footer>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Layouts;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
+import Layouts from "../Layouts/Layouts";
 // named export
   const router = createBrowserRouter([
-  {
-    path: "/",
+
+    {
+
+        path:"/",
+        element:<Layouts></Layouts>,
+        children:[
+              {
+    path: "/home",
     element:<Home></Home>
   },
+
+        ]
+    },
+
   {
     path:"/products",
     element:<Products></Products>
