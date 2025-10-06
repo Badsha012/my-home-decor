f80bdc62a31d7903274544f894b919ceb6b1ab37
Badsha012 <badshasheikh361@gmail.com>
2025-10-06 20:46:37 +0600

    add new file

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+// named export
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default router
