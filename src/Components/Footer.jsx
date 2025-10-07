adff71e21e6929723611441d10ac1ec599541489
Badsha012 <badshasheikh361@gmail.com>
2025-10-07 07:12:57 +0600

    nine commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,24 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-           <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10">
-  <nav>
-    <h6 className="footer-title">Services</h6>
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Company</h6>
+       <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+  <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav>
   <nav>
-    <h6 className="footer-title">Social</h6>
     <div className="grid grid-flow-col gap-4">
       <a>
         <svg
@@ -56,8 +46,10 @@ const Footer = () => {
       </a>
     </div>
   </nav>
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Home Decor</p>
+  </aside>
 </footer>
-        </div>
     );
 };
 
